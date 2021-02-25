4bdf8fb58db97eacd822ca6d16cab5f2cb3e0009
Jonathan-Welham <jwelham@csumb.edu>
2021-02-24 19:25:12 -0800

    heroku fix 11

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello world!'
+    # return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    app.run()
+    app.run(host='0.0.0.0', debug=False)
+    # app.run()
