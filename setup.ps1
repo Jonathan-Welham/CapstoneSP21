fd78e6662bb4bd8b64af615fedbcd36a5e055427
kyyyyy288 <kyyyyy288@gmail.com>
2021-03-10 13:41:36 +0800

    test

---
+++ setup.ps1
@@ -0,0 +1,8 @@
+$env:DB_USER = "zevztpizgh5r0fq2";
+$env:DB_PASSWORD = "lguiaftrt0st9sy8";
+$env:DB_HOST = "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$env:DB_PORT = "3306";
+$env:DB_DATABASE = "x2veux80tsk3mvxl";
+$env:MAIL_SERVER = 'smtp.gmail.com';
+$env:EMAIL_ADDRESS = 'capstoneg21@gmail.com';
+$env:EMAIL_PASSWORD = '5har3dPa55word!'
