aa0aa035e688f943af35d4fa9be5cc7f99f8a3ea
Jason Buckley <jbuckley@csumb.edu>
2021-04-21 00:57:17 -0700

    added route to get history of test runs by test id

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ from models import db, App, Test, Test_Type, Test_Run
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template, request, jsonify, abort
 
-app = Flask(__name__, static_url_path='', static_folder='./build', template_folder='./build')
+app = Flask(__name__, static_url_path='',
+            static_folder='./build', template_folder='./build')
 
 # SQLAlchemy configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', "")
@@ -14,29 +15,33 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-def send_mail():  
+
+def send_mail():
     mailer = Mailer()
-    mailer.set_message(subject='Daily Testing Status Report', body='Test body.............')
+    mailer.set_message(subject='Daily Testing Status Report',
+                       body='Test body.............')
     recepients = ['capstoneg21@gmail.com']
     mailer.send(recepients)
+
 
 # create schedule for mailing status report
 scheduler = BackgroundScheduler()
 scheduler.start()
 
 scheduler.add_job(
-    func=send_mail, 
-    id='mailing_status_report', 
-    name='Mail every weekday at 5PM', 
-    trigger='cron', 
-    day_of_week='mon-fri', 
-    hour=17, # 5PM
+    func=send_mail,
+    id='mailing_status_report',
+    name='Mail every weekday at 5PM',
+    trigger='cron',
+    day_of_week='mon-fri',
+    hour=17,  # 5PM
     minute=0,
-    second=0,  
+    second=0,
     replace_existing=True)
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
+
 
 @app.route('/api/post-tests', methods=['POST'])
 def post_tests():
@@ -65,7 +70,8 @@ def post_tests():
                 return jsonify({'success': False, 'message': "No test_type given"}), 404
 
             # Grab the id for test type if it is already in the db, or add the test type and get its id
-            test_type_info = Test_Type.query.filter_by(test_type=body['test_type']).first()
+            test_type_info = Test_Type.query.filter_by(
+                test_type=body['test_type']).first()
             if(test_type_info):
                 test_type_id = test_type_info.test_type_id
             else:
@@ -86,32 +92,37 @@ def post_tests():
                     tests_badly_formatted.append(test)
                     continue
 
-                test_name = test['test'] 
+                test_name = test['test']
                 execution_time = test['execution_time'] if 'execution_time' in test and test['execution_time'] else None
                 test_status = test['result'] if 'result' in test and test['result'] else None
                 entry_date = datetime.today()
 
                 # if the test is already in the database update its information
-                affected_row = Test.query.filter_by(test=test_name, app_id=app_id).update({'execution_time': execution_time, 'test_status': test_status, 'times_run': (Test.times_run + 1), 'entry_date': entry_date})
+                affected_row = Test.query.filter_by(test=test_name, app_id=app_id).update(
+                    {'execution_time': execution_time, 'test_status': test_status, 'times_run': (Test.times_run + 1), 'entry_date': entry_date})
                 temp_test_run = None
                 # if no rows were affected on the update then it is not already in the database so add it and create a new test_run entry
                 if(affected_row < 1):
-                    temp_test = Test(app_id, test_type_id, test['test'], test['execution_time'], entry_date, test['result'])
+                    temp_test = Test(
+                        app_id, test_type_id, test['test'], test['execution_time'], entry_date, test['result'])
                     db.session.add(temp_test)
                     db.session.flush()
                     test_id = temp_test.test_id
-                    temp_test_run = Test_Run(test_id, temp_test.execution_time, temp_test.entry_date, temp_test.test_status)
+                    temp_test_run = Test_Run(
+                        test_id, temp_test.execution_time, temp_test.entry_date, temp_test.test_status)
                 # find the test_id of the test and create a new test_run entry
                 else:
-                    test_id = db.session.query(Test.test_id).filter_by(test=test['test']).first()[0]
-                    temp_test_run = Test_Run(test_id, test['execution_time'], entry_date, test['result'])
+                    test_id = db.session.query(Test.test_id).filter_by(
+                        test=test['test']).first()[0]
+                    temp_test_run = Test_Run(
+                        test_id, test['execution_time'], entry_date, test['result'])
                 # add test_run entry
                 db.session.add(temp_test_run)
 
                 tests_entered = tests_entered + 1
             db.session.commit()
         except Exception as e:
-            print(f"error: {e}",flush=True)
+            print(f"error: {e}", flush=True)
             return jsonify({"success": False, "message": "Error: failed to to process data. Please try again"}), 200
         finally:
             db.session.close()
@@ -123,20 +134,24 @@ def post_tests():
         return jsonify({"success": True, "message": f"{tests_entered} tests have been logged out of {tests_total}", "tests_badly_formatted": tests_badly_formatted}), 200
     return jsonify({"success": True, "message": f"{tests_entered} tests have been logged out of {tests_total}"}), 200
 
+
 @app.route('/api/query-tests', methods=['GET'])
 def get_tests():
     # Query tests without any filters
     apply_filters = request.args['apply_filters'] if 'apply_filters' in request.args else False
     if not apply_filters:
         try:
-            raw_results = db.session.query(Test, App, Test_Type).select_from(Test).join(App).join(Test_Type).all()
-            query_results = list(map(lambda result: {**{'app': result[1].app, 'test_type': result[2].test_type}, **{c.name: str(getattr(result[0], c.name)) for c in result[0].__table__.columns}}, raw_results))
+            raw_results = db.session.query(Test, App, Test_Type).select_from(
+                Test).join(App).join(Test_Type).all()
+            query_results = list(map(lambda result: {**{'app': result[1].app, 'test_type': result[2].test_type}, **{
+                                 c.name: str(getattr(result[0], c.name)) for c in result[0].__table__.columns}}, raw_results))
             return jsonify({'success': True, 'message': 'Query processed', 'query_results': query_results})
         except:
             return jsonify({'success': False, 'message': 'Error processing query'})
-       
+
     # Make a dictionary of attr. to filter tests by after validating keys from query
-    test_query_pairs = {key: value for key, value in request.args.items() if hasattr(Test, key)}
+    test_query_pairs = {key: value for key,
+                        value in request.args.items() if hasattr(Test, key)}
     """ 
         All valid keys derived from query string for filtering tests: 
             test_id
@@ -167,21 +182,25 @@ def get_tests():
     test_type = request.args['test_type'] if 'test_type' in request.args else None
     if test_type is not None:
         try:
-            test_type_result = Test_Type.query.filter_by(test_type=test_type).first()
+            test_type_result = Test_Type.query.filter_by(
+                test_type=test_type).first()
             if test_type_result is None:
                 return jsonify({'success': False, 'message': f'No test type with matching name: {test_type}'})
             test_type_id = test_type_result.test_type_id
             test_query_pairs['test_type_id'] = test_type_id
         except:
             return jsonify({'success': False, 'message': f'Error processing query searching for test type with matching name: {test_type}'})
-    
+
     # Get tests using all processed filters
     try:
-        raw_results = db.session.query(Test, App, Test_Type).select_from(Test).filter_by(**test_query_pairs).join(App).join(Test_Type).all()
-        query_results = list(map(lambda result: {**{'app': result[1].app, 'test_type': result[2].test_type}, **{c.name: str(getattr(result[0], c.name)) for c in result[0].__table__.columns}}, raw_results))
+        raw_results = db.session.query(Test, App, Test_Type).select_from(
+            Test).filter_by(**test_query_pairs).join(App).join(Test_Type).all()
+        query_results = list(map(lambda result: {**{'app': result[1].app, 'test_type': result[2].test_type}, **{
+                             c.name: str(getattr(result[0], c.name)) for c in result[0].__table__.columns}}, raw_results))
         return jsonify({'success': True, 'message': 'Query processed', 'query_results': query_results})
     except:
         return jsonify({'success': False, 'message': 'Error processing query'})
+
 
 @app.route('/api/get-dashboard-info', methods=['GET'])
 def get_dashboard_info():
@@ -200,12 +219,14 @@ def get_dashboard_info():
             output["test_frequencies"] = {"dates": [], "counts": []}
 
             for entry in entries:
-                output["test_frequencies"]["dates"].append(str(entry[0].month) + "/" + str(entry[0].day))
+                output["test_frequencies"]["dates"].append(
+                    str(entry[0].month) + "/" + str(entry[0].day))
                 output["test_frequencies"]["counts"].append(entry[1])
 
         return jsonify(output), 200
     else:
         abort(404)
+
 
 @app.route('/api/get-test-frequencies', methods=['GET'])
 def get_test_frequencies_route():
@@ -215,13 +236,13 @@ def get_test_frequencies_route():
         if(request.args and request.args['app']):
             temp = get_test_frequencies(request.args['app'])
             if(temp[0]):
-               entries = temp[1]; 
+                entries = temp[1]
             else:
                 return jsonify({"success": False, "message": "invalid query"}), 400
         else:
             temp = get_test_frequencies(None)
             if(temp[0]):
-               entries = temp[1]; 
+                entries = temp[1]
             else:
                 return jsonify({"success": False, "message": "invalid query"}), 400
 
@@ -230,50 +251,88 @@ def get_test_frequencies_route():
 
         output = {"success": True, "counts": [], "dates": []}
         for entry in entries:
-            output["dates"].append(str(entry[0].month) + "/" + str(entry[0].day))
+            output["dates"].append(
+                str(entry[0].month) + "/" + str(entry[0].day))
             output["counts"].append(entry[1])
 
         return jsonify(output), 200
     else:
         abort(404)
 
-# Returns the test frequencies for a specific app, or if given no app returns test frequencies for all apps. 
-# The data is returned as a array of tuples where the first element in each tuple is the date and the second 
+
+# Get test history for a specific Test
+@app.route('/api/get-test-history', methods=['GET'])
+def get_test_history_route():
+    if request.method == "GET":
+        if(request.args and request.args['test_id']):
+            test_id = request.args['test_id']
+
+            try:
+                # Retrieves the test run history for a specific test by test id
+                tests = db.session.query(Test_Run.entry_date, Test_Run.execution_time,
+                                         Test_Run.test_status).filter(Test_Run.test_id == test_id).all()
+
+                output = []
+                for test in tests:
+                    output.append({
+                        "entry_date": test[0],
+                        "execution_time": test[1],
+                        "test_status": test[2]
+                    })
+
+                return jsonify(output)
+            except Exception as e:
+                print(f"error: {e}", flush=True)
+                return jsonify({"success": False, "message": "Error processing query"}), 400
+            finally:
+                db.session.close()
+        else:
+            return jsonify({"success": False, "message": "Invalid query"}), 400
+    else:
+        abort(404)
+
+
+# Returns the test frequencies for a specific app, or if given no app returns test frequencies for all apps.
+# The data is returned as a array of tuples where the first element in each tuple is the date and the second
 # element is the amount of tests run on that date.
 def get_test_frequencies(app):
     try:
         print(f"app: {app}", flush=True)
         if(app):
-            return (True, db.session.query(db.func.cast(Test.entry_date, db.Date), db.func.count(db.func.cast(Test.entry_date, db.Date))).join(App) \
-            .filter(App.app == app).group_by(db.func.cast(Test.entry_date, db.Date)).all()) 
+            return (True, db.session.query(db.func.cast(Test.entry_date, db.Date), db.func.count(db.func.cast(Test.entry_date, db.Date))).join(App)
+                    .filter(App.app == app).group_by(db.func.cast(Test.entry_date, db.Date)).all())
         else:
-            return (True, db.session.query(db.func.cast(Test.entry_date, db.Date), db.func.count(db.func.cast(Test.entry_date, db.Date))).join(App) \
-            .group_by(db.func.cast(Test.entry_date, db.Date)).all())
+            return (True, db.session.query(db.func.cast(Test.entry_date, db.Date), db.func.count(db.func.cast(Test.entry_date, db.Date))).join(App)
+                    .group_by(db.func.cast(Test.entry_date, db.Date)).all())
     except Exception as e:
         return (False,)
+
 
 # returns the names of all apps
 def get_apps():
     try:
         output = []
         for app in db.session.query(App.app).all():
-            output.append({"app" : app[0]})
-        
+            output.append({"app": app[0]})
+
         return (True, output)
     except:
         return (False,)
     finally:
         db.session.close()
 
+
 # returns the most recent rows of tests as an array of jsons containing test id, app name, test type, test, execution time,
 # entry date, test status, and times run in this order.
 def get_recent_tests():
     args = (Test.test_id, App.app, Test_Type.test_type, Test.test, Test.execution_time,
             Test.entry_date, Test.test_status, Test.times_run)
-    args_to_string = ["test_id", "app", "test_type", "test", "execution_time", "entry_date", "test_status", "times_run"]
+    args_to_string = ["test_id", "app", "test_type", "test",
+                      "execution_time", "entry_date", "test_status", "times_run"]
 
     try:
-        tests = db.session.query(*args).join(App).join(Test_Type).order_by(Test.entry_date).limit(50).all()
+        tests = db.session.query(
+            *args).join(App).join(Test_Type).order_by(Test.entry_date).limit(50).all()
         output = []
 
         # convert tests from an array format to an array of jsons
@@ -291,9 +350,11 @@ def get_recent_tests():
     finally:
         db.session.close()
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
+
 
 """
     An application factory for tethering a database to SQLAlchemy models.
@@ -307,13 +368,15 @@ def home():
         3. >>> db.create_all(app=create_app())
 """
 def create_app():
-    app = Flask(__name__, static_url_path='', static_folder='./build', template_folder='./build')
+    app = Flask(__name__, static_url_path='',
+                static_folder='./build', template_folder='./build')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', "")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     return app
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
